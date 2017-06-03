07475f9e3e9c9177ca0d9080102fd78ef7a0c8a2
risnawan <risnawanbudianto@gmail.com>
2017-06-03 23:46:50 +0700

    sms gateway

---
+++ src/smsgateway/SendMessage.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smsgateway;
+
+import org.smslib.AGateway;
+import org.smslib.IOutboundMessageNotification;
+import org.smslib.Library;
+import org.smslib.OutboundMessage;
+import org.smslib.Service;
+import org.smslib.modem.SerialModemGateway;
+
+
+public class SendMessage
+{
+        public void doIt() throws Exception
+        {
+                OutboundNotification outboundNotification = new OutboundNotification();
+                System.out.println("Example: Send message from a serial gsm modem.");
+                System.out.println(Library.getLibraryDescription());
+                System.out.println("Version: " + Library.getLibraryVersion());
+                SerialModemGateway gateway = new SerialModemGateway("", "COM9", 9600, "", "AirCard 312U");
+                gateway.setInbound(true);
+                gateway.setOutbound(true);
+                Service.getInstance().setOutboundMessageNotification(outboundNotification);
+                Service.getInstance().addGateway(gateway);
+                Service.getInstance().startService();
+                System.out.println();
+                System.out.println("Modem Information:");
+                System.out.println("  Manufacturer: " + gateway.getManufacturer());
+                System.out.println("  Model: " + gateway.getModel());
+                System.out.println("  Serial No: " + gateway.getSerialNo());
+                System.out.println("  SIM IMSI: " + gateway.getImsi());
+                System.out.println("  Signal Level: " + gateway.getSignalLevel() + " dBm");
+                System.out.println("  Battery Level: " + gateway.getBatteryLevel() + "%");
+                System.out.println();
+                OutboundMessage msg = new OutboundMessage("081382690461", "Hello from risnawan!");
+                Service.getInstance().sendMessage(msg);
+                System.out.println(msg);
+                System.out.println("Now Sleeping - Hit <enter> to terminate.");
+                System.in.read();
+                Service.getInstance().stopService();
+        }
+
+        public class OutboundNotification implements IOutboundMessageNotification
+        {
+                public void process(AGateway gateway, OutboundMessage msg)
+                {
+                        System.out.println("Outbound handler called from Gateway: " + gateway.getGatewayId());
+                        System.out.println(msg);
+                }
+        }
+
+        public static void main(String args[])
+        {
+                SendMessage app = new SendMessage();
+                try
+                {
+                        app.doIt();
+                }
+                catch (Exception e)
+                {
+                        e.printStackTrace();
+                }
+        }
+}
